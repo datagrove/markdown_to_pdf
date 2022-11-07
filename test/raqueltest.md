752c8c8689d871b81314adef3822bfa9cc387da6
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-11-07 13:54:51 -0600

    test for strong

---
+++ test/raqueltest.md
@@ -0,0 +1,9 @@
+# Hi! 
+
+## Test file H2
+
+I just love **bold text**
+
+It is just text
+
+:p
--- test/test.dart
+++ test/test.dart
@@ -2,5 +2,6 @@ import 'package:markdown_to_pdf/markdown_to_pdf.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  await mdtopdf("test/test.md", "test/test.pdf");
+  // await mdtopdf("test/test.md", "test/test.pdf");
+  await mdtopdf("test/raqueltest.md", "test/raqueltest.pdf");
 }
