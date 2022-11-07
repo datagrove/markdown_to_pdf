a9e0ffff5e1211391efc24f2bbbd5b5f9db6dee6
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-11-07 13:09:06 -0600

    second revert

--- test/test.dart
+++ test/test.dart
@@ -1,5 +1,5 @@
 import 'package:markdown_to_pdf/markdown_to_pdf.dart';
-// import 'package:test/test.dart'; 
+import 'package:test/test.dart';
 
 void main() async {
   await mdtopdf("test/test.md", "test/test.pdf");
